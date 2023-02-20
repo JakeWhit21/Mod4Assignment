703c353251899c50fe97ba732d5675cf3b6fd742
Jake <jdwhitmore3@gmail.com>
2023-02-20 13:54:38 -0600

    Multiple movie entry option. COMPLETED PROJECT

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 // See https://aka.ms/new-console-template for more information
 
 string file = "movies.txt";
+string continueResp;
 
 bool duplicate = false;
 
@@ -15,52 +16,73 @@ Console.WriteLine("Enter anything else to quit.");
 // input response
 string? resp = Console.ReadLine();
 
-if (resp == "1") {
+if (resp == "1")
+{
     if (File.Exists(file))
     {
+        do
+        {
+            Console.WriteLine("Enter movie ID: ");
 
-    Console.WriteLine("Enter movie ID: ");
-    
-        string movieIdInput = Console.ReadLine();
+            string movieIdInput = Console.ReadLine();
 
-        StreamReader sr = new StreamReader(file);
-        while (!sr.EndOfStream) {
-            string line = sr.ReadLine();
-            string[] movieValues = line.Split(',');
+            StreamReader sr = new StreamReader(file);
 
-            if (movieIdInput == movieValues[0]) {
-                logger.Error("This is a duplicate id");
-                duplicate = true;
+
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] movieValues = line.Split(',');
+
+                if (movieIdInput == movieValues[0])
+                {
+                    logger.Error("This is a duplicate id");
+                    duplicate = true;
+                }
+
             }
-            
-
-        }
-        sr.Close();
+            sr.Close();
 
             StreamWriter sw = new StreamWriter(file, append: true);
-        if (!duplicate) {
-            Console.WriteLine("Enter movie title: ");
-            string movieTitleInput = Console.ReadLine();
-            Console.WriteLine("Enter movie genre: ");
-            string movieGenreInput = Console.ReadLine();
 
-            //Add movie input to text file
+            if (!duplicate)
+            {
+                Console.WriteLine("Enter movie title: ");
+                string movieTitleInput = Console.ReadLine();
+                Console.WriteLine("Enter movie genre: ");
+                string movieGenreInput = Console.ReadLine();
+
+                //Add movie input to text file
+                sw.WriteLine("{0},{1},{2}", movieIdInput, movieTitleInput, movieGenreInput);
+            }
+
+            sw.Close();
+
+            Console.WriteLine("Enter another entry (Y for yes, N for no)?");
+            continueResp = Console.ReadLine().ToUpper();
             
-            sw.WriteLine("{0},{1},{2}", movieIdInput, movieTitleInput, movieGenreInput);
-        }
-        sw.Close();
-        
+            duplicate = false;
 
-    
+        } while (continueResp == "Y");
+
+
+
+
+
 
     }
 
-} else if (resp == "2") {
+}
+else if (resp == "2")
+{
 
-    if (File.Exists(file)) {
+    if (File.Exists(file))
+    {
         // read data from file
         StreamReader sr = new StreamReader(file);
-        while (!sr.EndOfStream) {
+        while (!sr.EndOfStream)
+        {
             string line = sr.ReadLine();
             string[] movieValues = line.Split(',');
             //grab movieid
